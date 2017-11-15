e7cb98ccce6b081bc50c23a074baab2c1ba5fceb
Griffin Stiller <gstiller5@Griffins-MacBook-Pro.local>
2017-11-14 22:17:46 -0700

    graph 2 complete

--- project2_2.py
+++ project2_2.py
@@ -7,11 +7,11 @@ data_lines = data_lines[652:] # takes out all the lines beginning with #
 data = []
 
 # separating the lines by the '|' character
-# and stripping off the '\n' character
+# and data_line_stringipping off the '\n' character
 for x in data_lines:
-    str = x.split('|')
-    str[-1] = str[-1].rstrip()
-    data.append(str)
+    data_line_string = x.split('|')
+    data_line_string[-1] = data_line_string[-1].rstrip()
+    data.append(data_line_string)
 
 as_list = set()
 for x in data:
@@ -31,4 +31,43 @@ for x in data:
     as_list1[as_list.index(int(x[0]))][1] += 1
 
 print(as_list1)
+
+bin1 = 0
+bin2_5 = 0
+bin5_100 = 0
+bin100_200 = 0
+bin200_1000 = 0
+bin1000_plus = 0
+as_list2 = []
+for x in as_list1:
+    as_list2.append(x[1])
+    if x[1] == 1:
+        bin1 += 1
+    elif x[1] < 5:
+        bin2_5 += 1
+    elif x[1] < 100:
+        bin5_100 += 1
+    elif x[1] < 200:
+        bin100_200 += 1
+    elif x[1] < 1000:
+        bin200_1000 += 1
+    else:
+        bin1000_plus += 1
+
+bin_list = [bin1, bin2_5, bin5_100, bin100_200, bin200_1000, bin1000_plus]
+bin_list_strings = ['1', '2-5', '5-100', '100-200', '200-1000', '1000+']
+
+print(as_list2)
+print(bin_list)
+print(sum(bin_list))
+total_bins_num = sum(bin_list)
+for i in range(len(bin_list)):
+    bin_list[i] = bin_list[i]/total_bins_num
+print(bin_list)
+plt.bar(range(len(bin_list)), bin_list)
+plt.xlabel('Bins')
+plt.ylabel('Frequency')
+plt.title('Graph 2 - AS Node Degree Distribution')
+plt.xticks(range(len(bin_list)), bin_list_strings)
+plt.show()
 
